6cc253a9dabbe73d6c776d094be8d0621876bad8
pankaj-o <er.pankaj2021@gmail.com>
2023-08-30 21:44:06 +0530

    image added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
-import Layout from '@/components/Layout'
+import backgroundImage1 from "../backgroudimage1.jpg"; // Make sure the filename matches
 
-export default function Home() {
+const styling = {
+  backgroundSize: 'cover',
+  width: '100%',
+  height: '100%',
+  color: 'red',
+  backgroundImage: `url('/backgroudimage.jpg')`,
+};
+
+function Home() {
   return (
-    <div>
-    <h3>my name is pankaj</h3>
+    <div className="team" style={styling}>
+      <p>here</p>
+      <img src="../backgroudimage1.jpg" alt="My Image" width={100} height={100} />
     </div>
-  )
+  );
 }
+
+export default Home;
